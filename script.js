865845b470fa9b4bf3dccf56a69dd8fd6719cf23
minimigui12 <00001074907930sp@al.educacao.sp.gov.br>
2024-09-24 00:44:06 -0300

    reajustes 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ avanca.forEach(button=> {
         const atual= document.querySelector('.ativo');
         const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo')
+        atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.edd('ativo');
     })
 })
